b740e448631a5d7dc7c88a4d642d9ef15c83fb36
chenyujin <3029795434@qq.com>
2026-02-16 11:09:47 +0800

    feat: 增加测试选项

---
+++ cmake/option.cmake
@@ -0,0 +1,2 @@
+option(BUILD_TEST "Build Test" ON)
+option(LOGGER_INSTALL "Install Logger bundle (Logger + yaml-tool + yaml-cpp + spdlog + test)" ON)
---
+++ test/main.cpp
@@ -0,0 +1,7 @@
+#include <logger/logger.h>
+
+int main()
+{
+    LOG_ERROR("Hello World - ", 1," - ", 1.11);
+    return 0;
+}
